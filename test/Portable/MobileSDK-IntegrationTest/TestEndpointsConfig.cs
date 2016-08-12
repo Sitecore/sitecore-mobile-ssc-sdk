47ae5b565232fc38548fc9a26afdc213ab2f9c1b
Igor <igor.kh.mail@gmail.com>
2016-08-12 10:06:39 +0300

    dead code removed

--- test/Portable/MobileSDK-IntegrationTest/TestEndpointsConfig.cs
+++ test/Portable/MobileSDK-IntegrationTest/TestEndpointsConfig.cs
@@ -6,6 +6,6 @@
     public const string InstanceUrl = "http://cms80u2.test24dk1.dk.sitecore.net/";
 
     //public const string InstanceUrl = "http://cms82rtm.test24dk1.dk.sitecore.net";
-    public const bool IsMediaHashesSupported = true;
+
   }
 }
