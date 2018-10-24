5d92883dda0f99ef3ca02fbfcc68516920b2f489
IGK <igk@sitecore.net>
2018-10-24 18:21:05 +0300

    migrating tests to .net standard

--- test/Portable/MobileSDK-IntegrationTest/TestEndpointsConfig.cs
+++ test/Portable/MobileSDK-IntegrationTest/TestEndpointsConfig.cs
@@ -3,7 +3,7 @@
   public class TestEndpointsConfig
   {
 
-    public const string InstanceUrl = "http://cms80u2.test24dk1.dk.sitecore.net/";
+    public const string InstanceUrl = "https://tst90170928.test24dk1.dk.sitecore.net";
 
     //public const string InstanceUrl = "http://cms82rtm.test24dk1.dk.sitecore.net";
 
